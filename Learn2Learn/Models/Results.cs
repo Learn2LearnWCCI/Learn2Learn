7ee2e892cdb01bba1009702617c3a9e7b2c85281
Zack Raycraft <raycraftzack@aol.com>
2017-03-09 11:55:53 -0500

    Initial Commit.

---
+++ Learn2Learn/Models/Results.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Learn2Learn.Models
+{
+    public class Results
+    {
+        [Key]
+        public int ID { get; set; }
+        public int AuditoryResult { get; set; }
+        public int VisualResult { get; set; }
+        public int KinestheticResult { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+
+
+    }
+}
